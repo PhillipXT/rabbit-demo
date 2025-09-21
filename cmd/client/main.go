95452184b0c1bbfe5ffde7f072a5f01d6de4d728
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-09-21 17:20:59 -0600

    Auto-create channel and queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/phillipxt/rabbit-demo/internal/gamelogic"
+	"github.com/phillipxt/rabbit-demo/internal/pubsub"
+	"github.com/phillipxt/rabbit-demo/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	cstr := "amqp://guest:guest@localhost:5672"
+
+	conn, err := amqp.Dial(cstr)
+	if err != nil {
+		log.Fatalf("Could not connect to RabbitMQ: %v", err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Game server connected to RabbitMQ!")
+
+	name, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Error getting username: %v", err)
+	}
+
+	queueName := fmt.Sprintf("pause.%s", name)
+	pubsub.CreateChannel(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
+
+	// Wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("RabbitMQ connection closed..")
 }
---
+++ internal/pubsub/create_channel.go
@@ -0,0 +1,36 @@
+package pubsub
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type simpleQueueType int
+
+const (
+	Durable simpleQueueType = iota
+	Transient
+)
+
+func CreateChannel(conn *amqp.Connection, exchange, queueName, key string, queueType simpleQueueType) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return &amqp.Channel{}, amqp.Queue{}, err
+	}
+
+	durable := queueType == Durable
+	autoDelete := queueType == Transient
+	exclusive := queueType == Transient
+	noWait := false
+
+	q, err := ch.QueueDeclare(queueName, durable, autoDelete, exclusive, noWait, nil)
+	if err != nil {
+		return &amqp.Channel{}, amqp.Queue{}, err
+	}
+
+	err = ch.QueueBind(queueName, key, exchange, noWait, nil)
+	if err != nil {
+		return &amqp.Channel{}, amqp.Queue{}, err
+	}
+
+	return ch, q, nil
+}
