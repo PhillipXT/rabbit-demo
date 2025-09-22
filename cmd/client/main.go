cbd44923d98dff67714c7975def99ef1bb05bbc5
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-09-22 00:13:52 -0600

    Added client command logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/phillipxt/rabbit-demo/internal/gamelogic"
 	"github.com/phillipxt/rabbit-demo/internal/pubsub"
@@ -33,10 +31,41 @@ func main() {
 	queueName := fmt.Sprintf("pause.%s", name)
 	pubsub.CreateChannel(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, pubsub.Transient)
 
-	// Wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	game := gamelogic.NewGameState(name)
+
+loop:
+	for {
+		cmd := gamelogic.GetInput()
+		if len(cmd) == 0 {
+			continue
+		}
+		switch cmd[0] {
+		case "spawn":
+			err := game.CommandSpawn(cmd)
+			if err != nil {
+				fmt.Println("Error:", err)
+				continue
+			}
+		case "move":
+			move, err := game.CommandMove(cmd)
+			if err != nil {
+				fmt.Println("Error:", err)
+				continue
+			}
+			fmt.Printf("move %v 1", move.ToLocation)
+		case "status":
+			game.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet")
+		case "quit":
+			gamelogic.PrintQuit()
+			break loop
+		default:
+			fmt.Println("Unknown command.")
+		}
+	}
 
 	fmt.Println("RabbitMQ connection closed..")
 }
